a884270dae63c26c61a7c496259ee1d91bcd4ace
pritam1813 <pritam1813@gmail.com>
2022-12-01 22:35:54 +0530

    Installed View Engine Ejs and Setup Sample view

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express Home controller</h1>');
+    //return res.end('<h1>Express Home controller</h1>');
+    return res.render('../views/Home', {title : 'Codesocial'});
 };
 
 module.exports.profile = function(req, res){
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+//setting view engine
+app.set('view engine', 'ejs');
+
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
